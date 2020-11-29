2dc56baa48f81b39d3a5dfa0fbde37f0c9289900
Niklas Mikael <nikke.larsson@gmail.com>
2020-11-29 17:09:42 +0200

    Cleared up the code a bit

--- mcservertool.sh
+++ mcservertool.sh
@@ -1,48 +1,73 @@
 #!/bin/bash
 
-# This script first downloads minecraft server from Mojangs official site.
-# It then creates a launch-script, that - when executed - initiates the server.
+# This script first downloads minecraft server 
+# from Mojangs official site.
+# It then creates a launch-script, that - when executed 
+# - initiates the server.
 
 # Download url
 URL=https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar
 
-# Download latest version of the server with wget
-# '--limit-rate'- flag is used just for making sure,
-# that the download won't use the full bandwidth,
-# which could cause temporary, slower connection for others
-# using the same network
-# Use 'sleep' for 2 seconds in case of accidental execution
-# '--progress'-flag is for configuring the progressbar of wget
+function countdown() {
+
 echo 'Initiating download in...'
 sleep 1
-echo '  ====='
-echo '  = 3 ='
-echo '  ====='
+echo '3'
 sleep 1
-echo '  ====='
-echo '  = 2 ='
-echo '  ====='
+echo '2'
 sleep 1
-echo '  ====='
-echo '  = 1 ='
-echo '  ====='
+echo '1'
 sleep 1
-wget --limit-rate=5m --progress=dot:mega --no-verbose --show-progress -O server.jar $(echo $URL)
 
-# Create launch script for launching the server quickly
-touch launch.sh
-# Enable execution rights
-chmod +x launch.sh
+}
 
-# Insert all necessary stuff into the launch-script
-echo '#!/bin/bash' > launch.sh
-printf '\n# Server launch settings\n' >> launch.sh
-echo 'java -Xmx1024M -Xms1024M -jar server.jar nogui' >> launch.sh
+# If server already exists, then do conditional stuff
+if [[ -e server.jar ]]
+then
+	echo '========================================================'
+	echo 'SERVER.JAR ALREADY EXISTS!'
+	echo '========================================================'
+	echo 'IF YOU ARE SURE THAT YOU WANT TO DOWNLOAD ANYWAY, IF FOR'
+	echo 'EXAMPLE NEWER VERSION OF THE SERVER IS AVAILABLE,'
+	echo 'THEN TYPE: yes or no'
+	echo '========================================================'
+	read -p 'Install anyway?: ' install_anyway
 
-echo ''
-echo '==========================================================='
-echo 'Execution script launch.sh created for launching the server'
-echo 'Use it by typing: ./launch.sh and the server will start!'
-echo 'If you wish to launch the server with more ram,'
-echo 'then edit the launch.sh'
-echo '==========================================================='
+	if [[ $install_anyway == 'yes' ]]
+	then
+		# Do countdown before excuting the download
+		countdown
+		wget --limit-rate=5m --progress=dot:mega --no-verbose --show-progress -O server.jar $(echo $URL)
+	fi
+
+	if [[ $install_anyway == 'no' ]]
+	then
+		# Quit
+		echo 'Mission aborted...'
+	fi
+
+else
+	# Do countdown before excuting the download
+	countdown
+	wget --limit-rate=5m --progress=dot:mega --no-verbose --show-progress -O server.jar $(echo $URL)
+
+	# Create launch script for launching the server quickly
+	touch launch.sh
+
+	# Enable execution rights
+	chmod +x launch.sh
+
+	# Insert all necessary stuff into the launch-script
+	echo '#!/bin/bash' > launch.sh
+	printf '\n# Server launch settings\n' >> launch.sh
+	echo 'java -Xmx1024M -Xms512M -jar server.jar nogui' >> launch.sh
+
+	echo ''
+	echo '==========================================================='
+	echo 'Execution script launch.sh created for launching the server'
+	echo 'Use it by typing: ./launch.sh and the server will start!'
+	echo 'If you wish to launch the server with more ram, then edit'
+	echo 'the launch.sh'
+	echo '==========================================================='
+fi
+
